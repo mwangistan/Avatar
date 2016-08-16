66176d64c31b20ac0e666bbd444f179afcbaadf0
stan <stan@stan-HP-G62-Notebook-PC.(none)>
2016-08-16 15:50:33 +0300

    Final commit

--- prime_numbers.py
+++ prime_numbers.py
@@ -20,37 +20,3 @@ def prime_num(n):
 
 prime_num(20)
 
-
-def fib():
-	#Ask for the end number of the sequence
-	end_number = raw_input("Enter the end number of the sequence: ")
-
-class PrimeChecker:
-  
-  #init method for all variables
-  def __init__(self, number=None):
-    self.number = number 
-    
-  #create a function to return true if number is prime
-  def is_prime(self):
-    #prime numbers start from 2
-    limit = self.number ** 0.5
-    
-    try:
-    
-      if self.number < 2:
-        return False
-      else:
-        divisor = 2
-        for i in range(2, self.number):
-          for h in range(2, limit):
-            if i % h == 0:
-              return False
-            else:
-              return True
-    except:
-      return False
-
-
-
-
